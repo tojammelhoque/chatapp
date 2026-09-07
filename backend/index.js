3579fa40d89b87bd18417ecac3db78111f889143
Tojammel Hoque <tojammelhoque375@gmail.com>
2026-09-07 23:08:14 +0530

    setup backend for chat app

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+username = signuptojammelhoque_db_user
+password = soOtM3jX8N6Zbk6L
