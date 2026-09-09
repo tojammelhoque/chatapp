58ca00e2c15744f231b35290e280370be1e61138
Tojammel Hoque <tojammelhoque375@gmail.com>
2026-09-09 18:18:33 +0530

    start frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+const App = () => {
+  return (
+    <div>
+      <h1>ChatApp</h1>
+      <header>
+        <Show when="signed-out">
+          <SignInButton mode="modal" />
+          <SignUpButton mode="modal" />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+    </div>
+  );
+};
+
+export default App;
