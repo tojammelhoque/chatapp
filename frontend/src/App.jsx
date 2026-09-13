c7a96e32afb89f47360148ac8add871e182dc5de
Tojammel Hoque <tojammelhoque375@gmail.com>
2026-09-13 10:04:33 +0530

    strat forntend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+// import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import { Navigate, Route, Routes } from "react-router";
+import { ThemeProvider } from "./context/ThemeContext";
+import { WallpaperProvider } from "./context/WallpaperContext";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
 const App = () => {
+  const { isSignedIn, isLoading } = useAuth();
+  if (!isLoading) return <p>Loading...</p>;
   return (
-    <div>
-      <h1>ChatApp</h1>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />
+            }
+          />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/react'
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/react";
+import { BrowserRouter } from "react-router";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider>
-    <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
-)
+);
